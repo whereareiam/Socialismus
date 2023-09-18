c3cba9426c8be799166470e323e40505e98b22ee
whereareiam <mmm.g032@gmail.com>
2023-09-18 08:37:30 +0200

    DI & Project configuration

--- src/main/java/me/whereareiam/socialismus/Socialismus.java
+++ src/main/java/me/whereareiam/socialismus/Socialismus.java
@@ -1,11 +1,16 @@
 package me.whereareiam.socialismus;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Socialismus extends JavaPlugin {
+    private Injector injector;
 
     @Override
-    public void onLoad() {}
+    public void onLoad() {
+        injector = Guice.createInjector(new SocialismusConfig());
+    }
 
     @Override
     public void onEnable() {}
---
+++ src/main/java/me/whereareiam/socialismus/SocialismusConfig.java
@@ -0,0 +1,10 @@
+package me.whereareiam.socialismus;
+
+import com.google.inject.AbstractModule;
+
+public class SocialismusConfig extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
