137f015e29ccead644452d9ff073e83147860480
whereareiam <mmm.g032@gmail.com>
2023-09-18 08:20:58 +0200

    Initial commit

---
+++ src/main/java/me/whereareiam/socialismus/Socialismus.java
@@ -0,0 +1,15 @@
+package me.whereareiam.socialismus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Socialismus extends JavaPlugin {
+
+    @Override
+    public void onLoad() {}
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
