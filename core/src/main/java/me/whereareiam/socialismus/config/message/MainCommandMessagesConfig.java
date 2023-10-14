ca7314937fd3fb786a976b4bf910acdafaa4859f
whereareiam <mmm.g032@gmail.com>
2023-10-14 18:33:01 +0200

    Chore: Add message formatting.

--- core/src/main/java/me/whereareiam/socialismus/config/message/MainCommandMessagesConfig.java
+++ core/src/main/java/me/whereareiam/socialismus/config/message/MainCommandMessagesConfig.java
@@ -6,6 +6,7 @@ public class MainCommandMessagesConfig {
     public String description = "Help command";
     public String commandHelpFormat = "  <yellow>{command}<gray>{subCommand} <dark_gray>- <white>{description}";
     public List<String> helpFormat = List.of(
+            " ",
             " <green><bold>Command help <reset><gray>[List of commands]",
             "{commands}",
             " "
