b4a29e300311b9bf299d0de2bb0def8372fc47f2
whereareiam <mmm.g032@gmail.com>
2023-10-15 17:14:16 +0200

    Chore: Documentation improvements.

--- core/src/main/java/me/whereareiam/socialismus/config/chat/ChatsConfig.java
+++ core/src/main/java/me/whereareiam/socialismus/config/chat/ChatsConfig.java
@@ -49,7 +49,7 @@ public class ChatsConfig extends YamlSerializable {
                     @CommentValue("                     **It is not necessary to specify**"),
                     @CommentValue(type = CommentValue.Type.TEXT),
                     @CommentValue(" message-format -    The symbol that you need to put in front of the message to send it to the desired chat."),
-                    @CommentValue("                     **It is not necessary to specify**"),
+                    @CommentValue("                     **It should be specified, you can use placeholders, formatting codes, letters and numbers**"),
                     @CommentValue(type = CommentValue.Type.TEXT),
                     @CommentValue(" hover-format -      Message that will be displayed when you hover over a message from the chat."),
                     @CommentValue("                     **It is not necessary to specify**"),
