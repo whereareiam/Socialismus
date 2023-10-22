955ae0cc96b163548a7e7f2d652c8025f50d2cd4
whereareiam <mmm.g032@gmail.com>
2023-10-22 16:44:34 +0200

    chore: add documentation for bubble chat.

--- core/src/main/java/me/whereareiam/socialismus/config/feature/bubblechat/BubbleChatFormatConfig.java
+++ core/src/main/java/me/whereareiam/socialismus/config/feature/bubblechat/BubbleChatFormatConfig.java
@@ -1,11 +1,76 @@
 package me.whereareiam.socialismus.config.feature.bubblechat;
 
+import net.elytrium.serializer.annotations.Comment;
+import net.elytrium.serializer.annotations.CommentValue;
+
 public class BubbleChatFormatConfig {
+
+    @Comment(
+            value = {
+                    @CommentValue(type = CommentValue.Type.NEW_LINE),
+                    @CommentValue(" The Start Message Format allows you to customise how the start message is displayed."),
+                    @CommentValue(" You can use PlaceholderAPI placeholders, MiniMessage formatting and internal"),
+                    @CommentValue(" {message} placeholder.")
+            },
+            at = Comment.At.PREPEND
+    )
     public String startMessageFormat = "%player_name%: {message}";
+
+    @Comment(
+            value = {
+                    @CommentValue(" This option is similar to the previous one, but is used for starting messages in the queue."),
+                    @CommentValue(" You can use PlaceholderAPI placeholders, MiniMessage formatting and internal"),
+                    @CommentValue(" {message} placeholder.")
+            },
+            at = Comment.At.PREPEND
+    )
     public String queueMessageFormat = "...{message}";
+
+    @Comment(
+            value = {
+                    @CommentValue(" If a message doesn't fit into a single bubble, it will be cut off and printed in the next"),
+                    @CommentValue(" bubble. Here you can adjust how the cut is displayed."),
+                    @CommentValue(" You can use PlaceholderAPI placeholders, MiniMessage formatting and internal"),
+                    @CommentValue(" {message} placeholder.")
+            },
+            at = Comment.At.PREPEND
+    )
     public String cutMessageFormat = "...";
+
+    @Comment(
+            value = {
+                    @CommentValue(" At the end of a single bubble or queue, you can add a message to the end."),
+                    @CommentValue(" You can use PlaceholderAPI placeholders, MiniMessage formatting and internal"),
+                    @CommentValue(" {message} placeholder.")
+            },
+            at = Comment.At.PREPEND
+    )
     public String endMessageFormat = ".";
+
+    @Comment(
+            value = {
+                    @CommentValue(" This option allows you to specify the color of the background. Doesn't work"),
+                    @CommentValue(" if opacity is set to 0. You must use the RGB format."),
+            },
+            at = Comment.At.PREPEND
+    )
     public String backgroundColor = "#FFFF00";
+
+    @Comment(
+            value = {
+                    @CommentValue(" Adjust the opacity of your background colour or turn off the background."),
+                    @CommentValue(" Values from 0 to 255 are available. (Set 0 to disable)"),
+            },
+            at = Comment.At.PREPEND
+    )
     public int backgroundOpacity = 0;
+
+    @Comment(
+            value = {
+                    @CommentValue(" This option allows you to add a beautiful shadow to your text."),
+                    @CommentValue(" If set to true, it will be displayed."),
+            },
+            at = Comment.At.PREPEND
+    )
     public boolean textShadow = false;
 }
--- core/src/main/java/me/whereareiam/socialismus/config/feature/bubblechat/BubbleChatSettingsConfig.java
+++ core/src/main/java/me/whereareiam/socialismus/config/feature/bubblechat/BubbleChatSettingsConfig.java
@@ -2,16 +2,100 @@ package me.whereareiam.socialismus.config.feature.bubblechat;
 
 import me.whereareiam.socialismus.integration.protocollib.entity.metadata.display.type.AlignmentType;
 import me.whereareiam.socialismus.integration.protocollib.entity.metadata.display.type.DisplayType;
+import net.elytrium.serializer.annotations.Comment;
+import net.elytrium.serializer.annotations.CommentValue;
 import org.joml.Vector3f;
 
 public class BubbleChatSettingsConfig {
+
+    @Comment(
+            value = {
+                    @CommentValue(type = CommentValue.Type.NEW_LINE),
+                    @CommentValue(" Alignment type specifies to which border side the message will be floated."),
+                    @CommentValue(" Available values: LEFT, RIGHT and CENTER."),
+            },
+            at = Comment.At.PREPEND
+    )
     public AlignmentType alignmentType = AlignmentType.CENTER;
+
+    @Comment(
+            value = {
+                    @CommentValue(" Display type is the way the bubble with the message follows the player's gaze."),
+                    @CommentValue(" Available values: FIXED, VERTICAL, HORIZONTAL and CENTER."),
+            },
+            at = Comment.At.PREPEND
+    )
     public DisplayType displayType = DisplayType.VERTICAL;
+
+
+    @Comment(
+            value = {
+                    @CommentValue(" The See Through option controls how the bubble will behave behind a structure."),
+                    @CommentValue(type = CommentValue.Type.TEXT),
+                    @CommentValue(" Available values:"),
+                    @CommentValue(" true - you will see the bubble behind a block (Armor stand behaviour)"),
+                    @CommentValue(" false - false - you will not see the bubble behind a block."),
+            },
+            at = Comment.At.PREPEND
+    )
     public boolean seeThrough = false;
+
+    @Comment(
+            value = {
+                    @CommentValue(" The line width options allow you to specify the width of a single line."),
+                    @CommentValue(" 100 corresponds to 16 symbols per line."),
+            },
+            at = Comment.At.PREPEND
+    )
     public int lineWidth = 100;
+
+    @Comment(
+            value = {
+                    @CommentValue(" Line Count allows you to limit the number of lines to display."),
+                    @CommentValue(" You can write a number from 1 to infinite here"),
+            },
+            at = Comment.At.PREPEND
+    )
     public int lineCount = 5;
+
+    @Comment(
+            value = {
+                    @CommentValue(" This setting represents the distance between a bubble message and the player's head."),
+                    @CommentValue(" You can set a value from 0 to infinite."),
+            },
+            at = Comment.At.PREPEND
+    )
     public int headDistance = 1;
+
+
+    @Comment(
+            value = {
+                    @CommentValue(" This setting allows you to adjust how much time is given to read a single symbol, resulting"),
+                    @CommentValue(" in the main display time being calculated for the entire bubble message."),
+                    @CommentValue(type = CommentValue.Type.TEXT),
+                    @CommentValue(" Example:"),
+                    @CommentValue("  \"I love bananas!\" - has 13 symbols, so we multiply 13 by 0.1 and we get 1.3 seconds"),
+                    @CommentValue("  to display this message.")
+            },
+            at = Comment.At.PREPEND
+    )
     public double timePerSymbol = 0.1;
+
+    @Comment(
+            value = {
+                    @CommentValue(" This setting controls a part of the behaviour of the previous one. Here you can set the"),
+                    @CommentValue(" minimum time a player has to read a message. For example, if we have calculated 1.3"),
+                    @CommentValue(" seconds, it would not be used, instead the 2 seconds from this option would be used."),
+            },
+            at = Comment.At.PREPEND
+    )
     public double minimumTime = 1.5;
+
+    @Comment(
+            value = {
+                    @CommentValue(" Display scale allows you to adjust the scale of the bubble."),
+            },
+            at = Comment.At.PREPEND
+    )
     public Vector3f displayScale = new Vector3f((float) 1.0, (float) 1.0, (float) 1.0);
 }
