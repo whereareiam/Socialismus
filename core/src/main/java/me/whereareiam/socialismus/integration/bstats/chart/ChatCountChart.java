e888fd86cedface144e6b984b8d330519258dcb2
whereareiam <mmm.g032@gmail.com>
2023-10-25 14:40:57 +0200

    chore: added new bStats charts

---
+++ core/src/main/java/me/whereareiam/socialismus/integration/bstats/chart/ChatCountChart.java
@@ -0,0 +1,35 @@
+package me.whereareiam.socialismus.integration.bstats.chart;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.whereareiam.socialismus.feature.FeatureLoader;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
+
+@Singleton
+public class ChatCountChart implements Chart {
+    private final FeatureLoader featureLoader;
+    private Metrics metrics;
+
+    @Inject
+    public ChatCountChart(FeatureLoader featureLoader) {
+        this.featureLoader = featureLoader;
+    }
+
+    @Override
+    public void setMetrics(Metrics metrics) {
+        this.metrics = metrics;
+    }
+
+    @Override
+    public void addChart() {
+        metrics.addCustomChart(
+                new SimplePie("chatCount", this::getData)
+        );
+    }
+
+    @Override
+    public String getData() {
+        return String.valueOf(featureLoader.getChatCount());
+    }
+}
