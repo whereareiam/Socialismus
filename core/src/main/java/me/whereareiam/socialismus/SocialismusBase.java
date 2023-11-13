30798f1a6c8db9d8666e39e33dbe00ff3f44e4cf
whereareiam <mmm.g032@gmail.com>
2023-11-13 13:24:35 +0100

    refactor: move duplicate code into one main class.

--- core/src/main/java/me/whereareiam/socialismus/SocialismusBase.java
+++ core/src/main/java/me/whereareiam/socialismus/SocialismusBase.java
@@ -1,7 +1,11 @@
 package me.whereareiam.socialismus;
 
 import com.google.inject.Injector;
+import me.whereareiam.socialismus.command.management.CommandRegistrar;
+import me.whereareiam.socialismus.config.ConfigManager;
 import me.whereareiam.socialismus.database.DatabaseManager;
+import me.whereareiam.socialismus.module.ModuleLoader;
+import me.whereareiam.socialismus.util.InfoPrinterUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class SocialismusBase extends JavaPlugin {
@@ -11,6 +15,15 @@ public abstract class SocialismusBase extends JavaPlugin {
     @Override
     public void onEnable() {
         version = getDescription().getVersion();
+
+        ConfigManager configManager = injector.getInstance(ConfigManager.class);
+        configManager.setDataFolder(getDataFolder().toPath());
+        configManager.reloadConfigs();
+
+        injector.getInstance(CommandRegistrar.class).registerCommands();
+        injector.getInstance(ModuleLoader.class).loadModules();
+
+        injector.getInstance(InfoPrinterUtil.class).printStartMessage();
     }
 
     @Override
--- paper/src/main/java/me/whereareiam/socialismus/SocialismusPaper.java
+++ paper/src/main/java/me/whereareiam/socialismus/SocialismusPaper.java
@@ -1,29 +1,15 @@
 package me.whereareiam.socialismus;
 
 import com.google.inject.Guice;
-import me.whereareiam.socialismus.command.management.CommandRegistrar;
-import me.whereareiam.socialismus.config.ConfigManager;
 import me.whereareiam.socialismus.listener.PaperListenerRegistrar;
-import me.whereareiam.socialismus.module.ModuleLoader;
-import me.whereareiam.socialismus.util.InfoPrinterUtil;
 
 public final class SocialismusPaper extends SocialismusBase {
 
     @Override
     public void onEnable() {
-        super.onEnable();
-
         injector = Guice.createInjector(new SocialismusPaperConfig(this));
-
-        ConfigManager configManager = injector.getInstance(ConfigManager.class);
-        configManager.setDataFolder(getDataFolder().toPath());
-        configManager.reloadConfigs();
-
-        injector.getInstance(CommandRegistrar.class).registerCommands();
-        injector.getInstance(ModuleLoader.class).loadModules();
-
-        injector.getInstance(InfoPrinterUtil.class).printStartMessage();
-
         injector.getInstance(PaperListenerRegistrar.class).registerListeners();
+
+        super.onEnable();
     }
 }
