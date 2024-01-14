5f0b26f31b2fe613a99483a6da2ad83b3edc4c50
whereareiam <mmm.g032@gmail.com>
2024-01-14 11:17:39 +0100

    chore(core): Convert legacy color codes to minimessage format.

--- core/src/main/java/me/whereareiam/socialismus/util/FormatterUtil.java
+++ core/src/main/java/me/whereareiam/socialismus/util/FormatterUtil.java
@@ -10,10 +10,15 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Singleton
 public class FormatterUtil {
     private final LoggerUtil loggerUtil;
     private final IntegrationManager integrationManager;
+
+    private final Map<String, String> colorMap = new HashMap<>();
 
     @Inject
     public FormatterUtil(LoggerUtil loggerUtil, IntegrationManager integrationManager) {
@@ -21,6 +26,29 @@ public class FormatterUtil {
         this.integrationManager = integrationManager;
 
         loggerUtil.trace("Initializing class: " + this);
+
+        colorMap.put("&0", "<black>");
+        colorMap.put("&1", "<dark_blue>");
+        colorMap.put("&2", "<dark_green>");
+        colorMap.put("&3", "<dark_aqua>");
+        colorMap.put("&4", "<dark_red>");
+        colorMap.put("&5", "<dark_purple>");
+        colorMap.put("&6", "<gold>");
+        colorMap.put("&7", "<gray>");
+        colorMap.put("&8", "<dark_gray>");
+        colorMap.put("&9", "<blue>");
+        colorMap.put("&a", "<green>");
+        colorMap.put("&b", "<aqua>");
+        colorMap.put("&c", "<red>");
+        colorMap.put("&d", "<light_purple>");
+        colorMap.put("&e", "<yellow>");
+        colorMap.put("&f", "<white>");
+        colorMap.put("&k", "<obfuscated>");
+        colorMap.put("&l", "<bold>");
+        colorMap.put("&m", "<strikethrough>");
+        colorMap.put("&n", "<underline>");
+        colorMap.put("&o", "<italic>");
+        colorMap.put("&r", "<reset>");
     }
 
     public String cleanMessage(String message) {
@@ -45,6 +73,8 @@ public class FormatterUtil {
         if (player != null)
             message = hookIntegration(player, message);
 
+        message = convertLegacyColorCodes(message);
+
         return miniMessage.deserialize(message);
     }
 
@@ -56,6 +86,15 @@ public class FormatterUtil {
 
                 loggerUtil.trace("Hooked with MESSAGING integration: " + formatterIntegration.getName());
             }
+        }
+
+        return message;
+    }
+
+    private String convertLegacyColorCodes(String message) {
+        for (Map.Entry<String, String> entry : colorMap.entrySet()) {
+            message = message.replace(entry.getKey(), entry.getValue());
+            message = message.replace(entry.getKey().replace("&", "§"), entry.getValue());
         }
 
         return message;
