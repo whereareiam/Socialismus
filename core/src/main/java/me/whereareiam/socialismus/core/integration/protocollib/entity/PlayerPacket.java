e4116a17fe1d32fdc93d8a157e657502b010108c
whereareiam <mmm.g032@gmail.com>
2024-01-18 10:59:07 +0100

    fix(core): fixed build.

---
+++ core/src/main/java/me/whereareiam/socialismus/core/integration/protocollib/entity/PlayerPacket.java
@@ -0,0 +1,28 @@
+package me.whereareiam.socialismus.core.integration.protocollib.entity;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.PlayerInfoData;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedGameProfile;
+import com.google.inject.Singleton;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+@Singleton
+public class PlayerPacket {
+	public PacketContainer createPlayerInfoPacket(String username) {
+		return new PacketContainer(PacketType.Play.Server.PLAYER_INFO) {{
+			getPlayerInfoActions().write(0, EnumSet.of(EnumWrappers.PlayerInfoAction.ADD_PLAYER));
+			getPlayerInfoDataLists().write(1, Collections.singletonList(new PlayerInfoData(
+					new WrappedGameProfile(UUID.randomUUID(), username),
+					1,
+					EnumWrappers.NativeGameMode.SURVIVAL,
+					WrappedChatComponent.fromText(username)
+			)));
+		}};
+	}
+}
