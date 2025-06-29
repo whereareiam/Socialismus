3ab02f33c5740fa82e32586145aaff4df2d047be
whereareiam <mmm.g032@gmail.com>
2025-06-29 19:42:14 +0200

    refactor: move welcome message to separate class

--- socialismus-common/src/main/java/me/whereareiam/socialismus/common/CommonSocialismus.java
+++ socialismus-common/src/main/java/me/whereareiam/socialismus/common/CommonSocialismus.java
@@ -1,7 +1,6 @@
 package me.whereareiam.socialismus.common;
 
 import com.google.inject.Injector;
-import me.whereareiam.socialismus.api.AnsiColor;
 import me.whereareiam.socialismus.api.Logger;
 import me.whereareiam.socialismus.api.Serializer;
 import me.whereareiam.socialismus.api.input.event.plugin.PluginInitializedEvent;
@@ -12,19 +11,13 @@ import me.whereareiam.socialismus.api.output.LoggingHelper;
 import me.whereareiam.socialismus.api.output.command.CommandService;
 import me.whereareiam.socialismus.api.output.listener.ListenerRegistrar;
 import me.whereareiam.socialismus.api.output.module.ModuleService;
-import me.whereareiam.socialismus.api.type.PlatformType;
-import me.whereareiam.socialismus.api.type.PluginType;
 import me.whereareiam.socialismus.api.util.EventUtil;
 import me.whereareiam.socialismus.common.chat.worker.FormatSelector;
 import me.whereareiam.socialismus.common.chat.worker.chatmessage.ChatSelector;
 import me.whereareiam.socialismus.common.chat.worker.chatmessage.RecipientResolver;
 import me.whereareiam.socialismus.common.chat.worker.chatmessage.RecipientSelector;
 import me.whereareiam.socialismus.common.container.ChatContainer;
-import me.whereareiam.socialismus.common.provider.IntegrationProvider;
-import me.whereareiam.socialismus.shared.Constants;
-
-import java.util.ArrayList;
-import java.util.List;
+import me.whereareiam.socialismus.common.printer.WelcomeBannerPrinter;
 
 public class CommonSocialismus {
 	private Injector injector;
@@ -50,7 +43,7 @@ public class CommonSocialismus {
 		injector.getInstance(CommandService.class).registerCommands();
 		injector.getInstance(ListenerRegistrar.class).registerListeners();
 
-		printWelcomeMessage();
+		injector.getInstance(WelcomeBannerPrinter.class).print();
 
 		injector.getInstance(Updater.class).start();
 		injector.getInstance(ModuleService.class).loadModules();
@@ -60,30 +53,5 @@ public class CommonSocialismus {
 
 	public void onDisable() {
 
-	}
-
-	private void printWelcomeMessage() {
-		LoggingHelper loggingHelper = injector.getInstance(LoggingHelper.class);
-		List<String> content = new ArrayList<>();
-
-		content.add(" ");
-		content.add(AnsiColor.CYAN + "  █▀ █▀▀   " + AnsiColor.RESET + "Socialismus v" + AnsiColor.GRAY + Constants.VERSION + AnsiColor.RESET);
-		content.add(AnsiColor.CYAN + "  ▄█ █▄▄   " + AnsiColor.RESET + "Platform: "
-				+ AnsiColor.GRAY
-				+ PlatformType.getType().toString()
-				+ " [" + PluginType.getType().toString() + "]"
-				+ AnsiColor.RESET
-		);
-		content.add(" ");
-		int commandCount = injector.getInstance(CommandService.class).getCommandCount();
-		content.add("  Loaded " + AnsiColor.CYAN + commandCount + AnsiColor.RESET + " command" + (commandCount == 1 ? "" : "s"));
-		int chatCount = injector.getInstance(ChatContainer.class).getChats().size();
-		content.add("  Loaded " + AnsiColor.CYAN + chatCount + AnsiColor.RESET + " chat" + (chatCount == 1 ? "" : "s"));
-		content.add(" ");
-		content.add("  Integrations:");
-		injector.getInstance(IntegrationProvider.class).get().forEach(integration -> content.add("    - " + AnsiColor.GREEN + integration.getName() + AnsiColor.RESET));
-		content.add(" ");
-
-		content.forEach(loggingHelper::info);
 	}
 }
