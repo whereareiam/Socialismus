e23a5092409fd59b381cda3e3a47d8e57d9123a8
whereareiam <mmm.g032@gmail.com>
2025-06-30 17:21:01 +0200

    fix(sync): add synchronization check in SyncServiceProvider

--- socialismus-adapter-module/src/main/java/me/whereareiam/socialismus/adapter/module/provider/sync/SyncServiceProvider.java
+++ socialismus-adapter-module/src/main/java/me/whereareiam/socialismus/adapter/module/provider/sync/SyncServiceProvider.java
@@ -5,6 +5,7 @@ import com.google.inject.Provider;
 import com.google.inject.ProvisionException;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
+import me.whereareiam.socialismus.api.model.config.Settings;
 import me.whereareiam.socialismus.api.output.resource.ResourceRegistry;
 import me.whereareiam.socialismus.api.output.resource.sync.SyncService;
 import me.whereareiam.socialismus.api.type.ResourceType;
@@ -13,14 +14,19 @@ import me.whereareiam.socialismus.api.type.ResourceType;
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class SyncServiceProvider implements Provider<SyncService> {
 	private final ResourceRegistry registry;
+	private final Provider<Settings> settings;
 
 	@Override
 	public SyncService get() {
-		Object svc = registry.get(ResourceType.SYNC)
-				.orElse(new DummySyncService());
+		if (!settings.get().getSynchronization().isEnabled())
+			return new DummySyncService();
 
-		if (!(svc instanceof SyncService))
+		Object svc = registry.get(ResourceType.SYNC)
+				.orElseThrow(() -> new ProvisionException("No SyncService registered!"));
+
+		if (!(svc instanceof SyncService)) {
 			throw new ProvisionException("Registered object is not a SyncService: " + svc.getClass());
+		}
 
 		return (SyncService) svc;
 	}
