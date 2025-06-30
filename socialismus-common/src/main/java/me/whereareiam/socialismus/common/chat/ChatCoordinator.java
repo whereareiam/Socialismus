46381e19b32a71b5d67ca6b6a1a742efd4d121ff
whereareiam <mmm.g032@gmail.com>
2025-06-30 16:18:28 +0200

    refactor(chat): add isBroadcastable method to determine message broadcast eligibility

--- socialismus-common/src/main/java/me/whereareiam/socialismus/common/chat/ChatCoordinator.java
+++ socialismus-common/src/main/java/me/whereareiam/socialismus/common/chat/ChatCoordinator.java
@@ -1,6 +1,7 @@
 package me.whereareiam.socialismus.common.chat;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import me.whereareiam.socialismus.api.input.chat.ChatCoordinationService;
@@ -8,6 +9,7 @@ import me.whereareiam.socialismus.api.input.container.ChatHistoryContainerServic
 import me.whereareiam.socialismus.api.input.event.chat.ChatBroadcastEvent;
 import me.whereareiam.socialismus.api.model.chat.message.ChatMessage;
 import me.whereareiam.socialismus.api.model.chat.message.FormattedChatMessage;
+import me.whereareiam.socialismus.api.model.config.Settings;
 import me.whereareiam.socialismus.api.util.EventUtil;
 import me.whereareiam.socialismus.common.chat.processor.ChatMessageProcessor;
 import me.whereareiam.socialismus.common.chat.processor.FormattedChatMessageProcessor;
@@ -19,6 +21,7 @@ public class ChatCoordinator implements ChatCoordinationService {
 	private final FormattedChatMessageProcessor formattedChatMessageProcessor;
 	private final ChatBroadcaster chatBroadcaster;
 	private final ChatHistoryContainerService chatHistoryContainer;
+	private final Provider<Settings> settings;
 
 	public FormattedChatMessage coordinate(ChatMessage chatMessage) {
 		chatMessage = chatMessageProcessor.process(chatMessage);
@@ -28,12 +31,23 @@ public class ChatCoordinator implements ChatCoordinationService {
 
 		EventUtil.callEvent(new ChatBroadcastEvent(formattedChatMessage, formattedChatMessage.isCancelled()), () -> {
 			formattedChatMessage.getSender().setLastChat(formattedChatMessage.getChat());
-			if (!formattedChatMessage.isVanillaSending())
+			System.out.println(isBroadcastable(formattedChatMessage));
+			if (isBroadcastable(formattedChatMessage))
 				chatBroadcaster.broadcast(formattedChatMessage);
 
 			chatHistoryContainer.addMessage(formattedChatMessage.getId(), formattedChatMessage);
 		});
 
 		return formattedChatMessage;
+	}
+
+	private boolean isBroadcastable(ChatMessage chatMessage) {
+		System.out.println(chatMessage.getOrigin());
+		System.out.println(settings.get().getSynchronization().getServer());
+		if (chatMessage.getOrigin() != null
+				&& !chatMessage.getOrigin().equals(settings.get().getSynchronization().getServer()))
+			return true;
+
+		return !chatMessage.isVanillaSending();
 	}
 }
