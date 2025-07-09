4eed9c2fd0878fac56d42e3121d1625d584410d4
whereareiam <mmm.g032@gmail.com>
2025-07-09 10:09:10 +0200

    fix(help): skip first entry in arguments list instead of comparing argument names

--- socialismus-adapter-command/src/main/java/me/whereareiam/socialismus/command/builder/HelpBuilder.java
+++ socialismus-adapter-command/src/main/java/me/whereareiam/socialismus/command/builder/HelpBuilder.java
@@ -16,62 +16,62 @@ import java.util.stream.Collectors;
 
 @Singleton
 public class HelpBuilder {
-    private final Provider<Settings> settings;
-    private final Provider<Messages> messages;
-    private final PaginationBuilder paginationBuilder;
+	private final Provider<Settings> settings;
+	private final Provider<Messages> messages;
+	private final PaginationBuilder paginationBuilder;
 
-    @Inject
-    public HelpBuilder(Provider<Settings> settings, Provider<Messages> messages, PaginationBuilder paginationBuilder) {
-        this.settings = settings;
-        this.messages = messages;
-        this.paginationBuilder = paginationBuilder;
-    }
+	@Inject
+	public HelpBuilder(Provider<Settings> settings, Provider<Messages> messages, PaginationBuilder paginationBuilder) {
+		this.settings = settings;
+		this.messages = messages;
+		this.paginationBuilder = paginationBuilder;
+	}
 
-    public String buildHelpMessage(Collection<Command<DummyPlayer>> allowedCommands, int page) {
-        String message = String.join("\n", messages.get().getCommands().getHelpCommand().getFormat());
+	public String buildHelpMessage(Collection<Command<DummyPlayer>> allowedCommands, int page) {
+		String message = String.join("\n", messages.get().getCommands().getHelpCommand().getFormat());
 
-        if (message.contains("{commands}")) message = buildCommandList(allowedCommands, message, page);
-        if (message.contains("{pagination}")) message = paginationBuilder.build(message, allowedCommands.size(), page);
+		if (message.contains("{commands}")) message = buildCommandList(allowedCommands, message, page);
+		if (message.contains("{pagination}")) message = paginationBuilder.build(message, allowedCommands.size(), page);
 
-        return message;
-    }
+		return message;
+	}
 
-    private String buildCommandList(Collection<Command<DummyPlayer>> commands, String message, int page) {
-        if (commands.isEmpty())
-            return message.replace("{commands}", messages.get().getCommands().getHelpCommand().getNoCommands());
+	private String buildCommandList(Collection<Command<DummyPlayer>> commands, String message, int page) {
+		if (commands.isEmpty())
+			return message.replace("{commands}", messages.get().getCommands().getHelpCommand().getNoCommands());
 
-        long skip = (long) (page - 1) * settings.get().getMisc().getCommandsPerPage();
+		long skip = (long) (page - 1) * settings.get().getMisc().getCommandsPerPage();
 
-        List<String> commandDescriptions = commands.stream().skip(skip).limit(settings.get().getMisc().getCommandsPerPage()).map(command -> {
-            String commandFormat = messages.get().getCommands().getHelpCommand().getCommandFormat();
-            return commandFormat.replace("{command}", command.rootComponent().name())
-                    .replace("{arguments}", formatCommandArguments(command.rootComponent().name(), command.nonFlagArguments()))
-                    .replace("{description}", command.commandDescription().description().textDescription());
-        }).collect(Collectors.toList());
+		List<String> commandDescriptions = commands.stream().skip(skip).limit(settings.get().getMisc().getCommandsPerPage()).map(command -> {
+			String commandFormat = messages.get().getCommands().getHelpCommand().getCommandFormat();
+			return commandFormat.replace("{command}", command.rootComponent().name())
+					.replace("{arguments}", formatCommandArguments(command.rootComponent().name(), command.nonFlagArguments()))
+					.replace("{description}", command.commandDescription().description().textDescription());
+		}).collect(Collectors.toList());
 
-        String commandsString = String.join("\n", commandDescriptions);
-        return message.replace("{commands}", commandsString);
-    }
+		String commandsString = String.join("\n", commandDescriptions);
+		return message.replace("{commands}", commandsString);
+	}
 
-    private String formatCommandArguments(String commandName, List<? extends CommandComponent<?>> arguments) {
-        if (arguments.isEmpty()) return "";
+	private String formatCommandArguments(String commandName, List<? extends CommandComponent<?>> arguments) {
+		if (arguments.isEmpty()) return "";
 
-        CommandMessages commandMessages = messages.get().getCommands();
+		CommandMessages commandMessages = messages.get().getCommands();
 
-        CommandMessages.Format messageFormat = messages.get().getCommands().getFormat();
-        return arguments.parallelStream()
-                .filter(argument -> !argument.name().equals(commandName))
-                .map(argument -> switch (argument.type()) {
-                    case REQUIRED_VARIABLE ->
-                            messageFormat.getArgument().replace("{argument}", commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name());
-                    case OPTIONAL_VARIABLE ->
-                            messageFormat.getOptionalArgument().replace("{argument}", commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name());
-                    default ->
-                            commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name();
-                })
-                .collect(Collectors.collectingAndThen(
-                        Collectors.joining(" "),
-                        formattedArguments -> formattedArguments.isEmpty() ? "" : " " + formattedArguments
-                ));
-    }
+		CommandMessages.Format messageFormat = messages.get().getCommands().getFormat();
+		return arguments.parallelStream()
+				.skip(1)
+				.map(argument -> switch (argument.type()) {
+					case REQUIRED_VARIABLE ->
+							messageFormat.getArgument().replace("{argument}", commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name());
+					case OPTIONAL_VARIABLE ->
+							messageFormat.getOptionalArgument().replace("{argument}", commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name());
+					default ->
+							commandMessages.getArguments().get(argument.name()) != null ? commandMessages.getArguments().get(argument.name()) : argument.name();
+				})
+				.collect(Collectors.collectingAndThen(
+						Collectors.joining(" "),
+						formattedArguments -> formattedArguments.isEmpty() ? "" : " " + formattedArguments
+				));
+	}
 }
--- socialismus-adapter-config/src/main/java/me/whereareiam/socialismus/adapter/config/template/MessagesTemplate.java
+++ socialismus-adapter-config/src/main/java/me/whereareiam/socialismus/adapter/config/template/MessagesTemplate.java
@@ -10,91 +10,93 @@ import java.util.Map;
 
 @Singleton
 public class MessagesTemplate implements DefaultConfig<Messages> {
-    @Override
-    public Messages getDefault() {
-        Messages messages = new Messages();
+	@Override
+	public Messages getDefault() {
+		Messages messages = new Messages();
 
-        // Default values
-        messages.setPrefix("<gold>ꜱᴏᴄɪᴀʟɪꜱᴍᴜꜱ <dark_gray>| ");
-        CommandMessages commandMessages = new CommandMessages();
-        commandMessages.setCooldown("{prefix}<white>You must wait <gray>{time} seconds</gray> before using this command again</white>");
-        commandMessages.setCancelled("{prefix}<white>Command execution has been <red>cancelled</red></white>");
-        commandMessages.setNoPermission("{prefix}<white>You don't have \"<gray>{content}</gray>\" permission to use this command.</white>");
-        commandMessages.setExecutionError("{prefix}<white>An error occurred while executing the command:</white> <gray>{content}</gray>");
+		// Default values
+		messages.setPrefix("<gold>ꜱᴏᴄɪᴀʟɪꜱᴍᴜꜱ <dark_gray>| ");
+		CommandMessages commandMessages = new CommandMessages();
+		commandMessages.setCooldown("{prefix}<white>You must wait <gray>{time} seconds</gray> before using this command again</white>");
+		commandMessages.setCancelled("{prefix}<white>Command execution has been <red>cancelled</red></white>");
+		commandMessages.setNoPermission("{prefix}<white>You don't have \"<gray>{content}</gray>\" permission to use this command.</white>");
+		commandMessages.setExecutionError("{prefix}<white>An error occurred while executing the command:</white> <gray>{content}</gray>");
 
-        commandMessages.setInvalidSyntax("{prefix}<white>Invalid syntax, please use:</white> <yellow>/{content}</yellow>");
-        commandMessages.setInvalidSyntaxBoolean("{prefix}<white>You tried to use <gray>{content}</gray> as a boolean, but it's not a valid value, please use <green>true</green> or <red>false</red>.</white>");
-        commandMessages.setInvalidSyntaxNumber("{prefix}<white>You tried to use <gray>{content}</gray> as a number, but it's not a valid value, please use a valid number.</white>");
-        commandMessages.setInvalidSyntaxString("{prefix}<white>You tried to use <gray>{content}</gray> as a string, but it's not a valid value, please use a valid string.</white>");
+		commandMessages.setInvalidSyntax("{prefix}<white>Invalid syntax, please use:</white> <yellow>/{content}</yellow>");
+		commandMessages.setInvalidSyntaxBoolean("{prefix}<white>You tried to use <gray>{content}</gray> as a boolean, but it's not a valid value, please use <green>true</green> or <red>false</red>.</white>");
+		commandMessages.setInvalidSyntaxNumber("{prefix}<white>You tried to use <gray>{content}</gray> as a number, but it's not a valid value, please use a valid number.</white>");
+		commandMessages.setInvalidSyntaxString("{prefix}<white>You tried to use <gray>{content}</gray> as a string, but it's not a valid value, please use a valid string.</white>");
 
-        commandMessages.setArguments(Map.of(
-                "id", "ID",
-                "bool", "true/false",
-                "page", "Page",
-                "context", "Name/ID/Amount"
-        ));
+		commandMessages.setArguments(Map.of(
+				"id", "ID",
+				"bool", "true/false",
+				"page", "Page",
+				"context", "Name/ID/Amount",
+				"recipient", "Recipient",
+				"message", "Message"
+		));
 
-        CommandMessages.Format format = new CommandMessages.Format();
-        format.setFormat("<gray>{command}</gray>");
-        format.setArgument("<gray>[{argument}]</gray>");
-        format.setOptionalArgument("<gray>({argument})</gray>");
+		CommandMessages.Format format = new CommandMessages.Format();
+		format.setFormat("<gray>{command}</gray>");
+		format.setArgument("<gray>[{argument}]</gray>");
+		format.setOptionalArgument("<gray>({argument})</gray>");
 
-        commandMessages.setFormat(format);
+		commandMessages.setFormat(format);
 
-        CommandMessages.Pagination pagination = new CommandMessages.Pagination();
-        pagination.setShowPaginationIfOnePage(false);
-        pagination.setFormat("\n {previous}<white>Pagination</white> <gray>[{current}/{max}]</gray>{next} \n");
-        pagination.setShowPreviousEvenIfFirst(false);
-        pagination.setPreviousTagFormat("<red><click:run_command:/social help {previousPage}>«</red> ");
-        pagination.setShowNextEvenIfLast(false);
-        pagination.setNextTagFormat(" <green><click:run_command:/social help {nextPage}>»</green>");
+		CommandMessages.Pagination pagination = new CommandMessages.Pagination();
+		pagination.setShowPaginationIfOnePage(false);
+		pagination.setFormat("\n {previous}<white>Pagination</white> <gray>[{current}/{max}]</gray>{next} \n");
+		pagination.setShowPreviousEvenIfFirst(false);
+		pagination.setPreviousTagFormat("<red><click:run_command:/social help {previousPage}>«</red> ");
+		pagination.setShowNextEvenIfLast(false);
+		pagination.setNextTagFormat(" <green><click:run_command:/social help {nextPage}>»</green>");
 
-        commandMessages.setPagination(pagination);
+		commandMessages.setPagination(pagination);
 
-        CommandMessages.HelpCommand helpCommand = new CommandMessages.HelpCommand();
-        helpCommand.setFormat(List.of(" ", "<gold><bold> Socialismus</bold> <white>Command help", " ", "{commands}", "{pagination}"));
-        helpCommand.setCommandFormat(" <yellow>/{command}{arguments}</yellow> <dark_gray>- <white>{description}");
-        helpCommand.setNoCommands("  <red>No commands found</red>");
+		CommandMessages.HelpCommand helpCommand = new CommandMessages.HelpCommand();
+		helpCommand.setFormat(List.of(" ", "<gold><bold> Socialismus</bold> <white>Command help", " ", "{commands}", "{pagination}"));
+		helpCommand.setCommandFormat(" <yellow>/{command}{arguments}</yellow> <dark_gray>- <white>{description}");
+		helpCommand.setNoCommands("  <red>No commands found</red>");
 
-        commandMessages.setHelpCommand(helpCommand);
+		commandMessages.setHelpCommand(helpCommand);
 
-        CommandMessages.DebugCommand debugCommand = new CommandMessages.DebugCommand();
-        debugCommand.setFormat(List.of(
-                " ",
-                "<gold><bold> Socialismus</bold> <white>Debug",
-                "<white>  Server version: <gray>{serverVersion}</gray>",
-                "<white>  Plugin version: <gray>{pluginVersion}</gray>",
-                " ",
-                "<white>  Server platform: <aqua>{serverPlatform}</aqua>",
-                "<white>  Plugin platform: <aqua>{pluginPlatform}</aqua>",
-                " ",
-                "<white>  Java version: <yellow>{javaVersion}</yellow>",
-                "<white>  Operating system: <yellow>{os}</yellow>",
-                " "
-        ));
+		CommandMessages.DebugCommand debugCommand = new CommandMessages.DebugCommand();
+		debugCommand.setFormat(List.of(
+				" ",
+				"<gold><bold> Socialismus</bold> <white>Debug",
+				"<white>  Server version: <gray>{serverVersion}</gray>",
+				"<white>  Plugin version: <gray>{pluginVersion}</gray>",
+				" ",
+				"<white>  Server platform: <aqua>{serverPlatform}</aqua>",
+				"<white>  Plugin platform: <aqua>{pluginPlatform}</aqua>",
+				" ",
+				"<white>  Java version: <yellow>{javaVersion}</yellow>",
+				"<white>  Operating system: <yellow>{os}</yellow>",
+				" "
+		));
 
-        commandMessages.setDebugCommand(debugCommand);
+		commandMessages.setDebugCommand(debugCommand);
 
-        CommandMessages.ReloadCommand reloadCommand = new CommandMessages.ReloadCommand();
-        reloadCommand.setReloading("{prefix}<white>Reloading configuration, some features may still require a server restart...</white>");
-        reloadCommand.setReloaded("{prefix}<white>Configuration reloaded <green>successfully</green>!</white>");
-        reloadCommand.setException("{prefix}<white>An error occurred while reloading the configuration:</white> <gray>{exception}</gray>");
+		CommandMessages.ReloadCommand reloadCommand = new CommandMessages.ReloadCommand();
+		reloadCommand.setReloading("{prefix}<white>Reloading configuration, some features may still require a server restart...</white>");
+		reloadCommand.setReloaded("{prefix}<white>Configuration reloaded <green>successfully</green>!</white>");
+		reloadCommand.setException("{prefix}<white>An error occurred while reloading the configuration:</white> <gray>{exception}</gray>");
 
-        commandMessages.setReloadCommand(reloadCommand);
+		commandMessages.setReloadCommand(reloadCommand);
 
-        CommandMessages.ClearCommand clearCommand = new CommandMessages.ClearCommand();
-        clearCommand.setBypassUser("{prefix}<white>You can't clear chat history from this user</white>");
-        clearCommand.setNoUserHistory("{prefix}<white>No chat history found for user <gray>{playerName}</gray></white>");
-        clearCommand.setNoIdHistory("{prefix}<white>No chat history found for ID <gray>{id}</gray></white>");
-        clearCommand.setNoHistory("{prefix}<white>No chat history found</white>");
-        clearCommand.setNotEnoughHistory("{prefix}<white>Not enough chat history entries to clear</white>");
-        clearCommand.setCleared("{prefix}<white>Successfully deleted message from chat history</white>");
-        clearCommand.setClearedAmount("{prefix}<white>Successfully cleared <gray>{amount}</gray>x chat history entries</white>");
+		CommandMessages.ClearCommand clearCommand = new CommandMessages.ClearCommand();
+		clearCommand.setBypassUser("{prefix}<white>You can't clear chat history from this user</white>");
+		clearCommand.setNoUserHistory("{prefix}<white>No chat history found for user <gray>{playerName}</gray></white>");
+		clearCommand.setNoIdHistory("{prefix}<white>No chat history found for ID <gray>{id}</gray></white>");
+		clearCommand.setNoHistory("{prefix}<white>No chat history found</white>");
+		clearCommand.setNotEnoughHistory("{prefix}<white>Not enough chat history entries to clear</white>");
+		clearCommand.setCleared("{prefix}<white>Successfully deleted message from chat history</white>");
+		clearCommand.setClearedAmount("{prefix}<white>Successfully cleared <gray>{amount}</gray>x chat history entries</white>");
 
-        commandMessages.setClearCommand(clearCommand);
+		commandMessages.setClearCommand(clearCommand);
 
-        messages.setCommands(commandMessages);
+		messages.setCommands(commandMessages);
 
-        return messages;
-    }
+		return messages;
+	}
 }
