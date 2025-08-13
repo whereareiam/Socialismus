04452cb9f41961f3981e52cc62aac436107ac795
whereareiam <mmm.g032@gmail.com>
2025-08-13 12:46:30 +0200

    fix(common): compare servers instead of location when on proxy

--- socialismus-common/src/main/java/me/whereareiam/socialismus/common/chat/worker/base/RecipientSelector.java
+++ socialismus-common/src/main/java/me/whereareiam/socialismus/common/chat/worker/base/RecipientSelector.java
@@ -16,6 +16,7 @@ import me.whereareiam.socialismus.api.model.player.DummyPlayer;
 import me.whereareiam.socialismus.api.model.serializer.SerializerContent;
 import me.whereareiam.socialismus.api.model.serializer.SerializerPlaceholder;
 import me.whereareiam.socialismus.api.output.PlatformInteractor;
+import me.whereareiam.socialismus.api.type.PlatformType;
 import me.whereareiam.socialismus.api.type.chat.Participants;
 import me.whereareiam.socialismus.api.util.EventUtil;
 import me.whereareiam.socialismus.common.requirement.RequirementEvaluator;
@@ -26,83 +27,86 @@ import java.util.stream.Collectors;
 
 @Singleton
 public class RecipientSelector {
-	private final RequirementEvaluator requirementEvaluator;
-	private final Provider<ChatSettings> settings;
-	private final Provider<ChatMessages> messages;
-	private final PlatformInteractor interactor;
+    private final RequirementEvaluator requirementEvaluator;
+    private final Provider<ChatSettings> settings;
+    private final Provider<ChatMessages> messages;
+    private final PlatformInteractor interactor;
 
-	@Inject
-	public RecipientSelector(
-			WorkerProcessor<ChatMessage> workerProcessor,
-			RequirementEvaluator requirementEvaluator,
-			Provider<ChatSettings> settings,
-			Provider<ChatMessages> messages,
-			PlatformInteractor interactor
-	) {
-		this.requirementEvaluator = requirementEvaluator;
-		this.settings = settings;
-		this.messages = messages;
-		this.interactor = interactor;
+    @Inject
+    public RecipientSelector(
+            WorkerProcessor<ChatMessage> workerProcessor,
+            RequirementEvaluator requirementEvaluator,
+            Provider<ChatSettings> settings,
+            Provider<ChatMessages> messages,
+            PlatformInteractor interactor
+    ) {
+        this.requirementEvaluator = requirementEvaluator;
+        this.settings = settings;
+        this.messages = messages;
+        this.interactor = interactor;
 
-		workerProcessor.addWorker(new Worker<>(this::selectRecipients, 100, true, false));
-	}
+        workerProcessor.addWorker(new Worker<>(this::selectRecipients, 100, true, false));
+    }
 
-	private ChatMessage selectRecipients(ChatMessage chatMessage) {
-		if (chatMessage.getChat() == null) {
-			chatMessage.setCancelled(true);
-			return chatMessage;
-		}
+    private ChatMessage selectRecipients(ChatMessage chatMessage) {
+        if (chatMessage.getChat() == null) {
+            chatMessage.setCancelled(true);
+            return chatMessage;
+        }
 
-		Chat chat = chatMessage.getChat();
-		DummyPlayer sender = chatMessage.getSender();
+        Chat chat = chatMessage.getChat();
+        DummyPlayer sender = chatMessage.getSender();
 
-		int oldRecipients = chatMessage.getRecipients().size();
-		Set<DummyPlayer> recipients = chatMessage.getRecipients();
+        int oldRecipients = chatMessage.getRecipients().size();
+        Set<DummyPlayer> recipients = chatMessage.getRecipients();
 
-		if (chat.getParameters().getType().isLocal()) {
-			recipients = recipients.parallelStream()
-					.filter(recipient -> isInSameRealm(sender, recipient))
-					.filter(recipient -> isWithinRadius(sender, recipient, chat.getParameters().getRadius()))
-					.filter(recipient -> checkRequirements(chat, recipient))
-					.collect(Collectors.toSet());
+        if (chat.getParameters().getType().isLocal()) {
+            recipients = recipients.parallelStream()
+                    .filter(recipient -> isInSameRealm(sender, recipient))
+                    .filter(recipient -> isWithinRadius(sender, recipient, chat.getParameters().getRadius()))
+                    .filter(recipient -> checkRequirements(chat, recipient))
+                    .collect(Collectors.toSet());
 
-			if (recipients.size() <= 1 && settings.get().isNotifyNoNearbyPlayers()) {
-				sender.sendMessage(Serializer.serialize(new SerializerContent(sender, List.of(new SerializerPlaceholder(
-						"{radius}",
-						String.valueOf(chat.getParameters().getRadius()))),
-						messages.get().getNoNearbyPlayers())
-				));
-				chatMessage.setCancelled(true);
-				return chatMessage;
-			}
-		} else {
-			recipients = recipients.stream()
-					.filter(recipient -> checkRequirements(chat, recipient))
-					.collect(Collectors.toSet());
-		}
+            if (recipients.size() <= 1 && settings.get().isNotifyNoNearbyPlayers()) {
+                sender.sendMessage(Serializer.serialize(new SerializerContent(sender, List.of(new SerializerPlaceholder(
+                        "{radius}",
+                        String.valueOf(chat.getParameters().getRadius()))),
+                        messages.get().getNoNearbyPlayers())
+                ));
+                chatMessage.setCancelled(true);
+                return chatMessage;
+            }
+        } else {
+            recipients = recipients.stream()
+                    .filter(recipient -> checkRequirements(chat, recipient))
+                    .collect(Collectors.toSet());
+        }
 
-		RecipientsSelectedEvent event = new RecipientsSelectedEvent(chatMessage, recipients, chatMessage.isCancelled());
-		EventUtil.callEvent(event, () -> chatMessage.setRecipients(event.getNewRecipients()));
-		Logger.debug("Recipients before: " + oldRecipients + ", after: " + event.getNewRecipients().size());
+        RecipientsSelectedEvent event = new RecipientsSelectedEvent(chatMessage, recipients, chatMessage.isCancelled());
+        EventUtil.callEvent(event, () -> chatMessage.setRecipients(event.getNewRecipients()));
+        Logger.debug("Recipients before: " + oldRecipients + ", after: " + event.getNewRecipients().size());
 
-		if (recipients.isEmpty()) chatMessage.setCancelled(true);
-		if (recipients.size() <= 1 && settings.get().isNotifyNoPlayers() && chat.getParameters().getType().isGlobal()) {
-			sender.sendMessage(Serializer.serialize(sender, messages.get().getNoPlayers()));
-			chatMessage.setCancelled(true);
-		}
+        if (recipients.isEmpty()) chatMessage.setCancelled(true);
+        if (recipients.size() <= 1 && settings.get().isNotifyNoPlayers() && chat.getParameters().getType().isGlobal()) {
+            sender.sendMessage(Serializer.serialize(sender, messages.get().getNoPlayers()));
+            chatMessage.setCancelled(true);
+        }
 
-		return chatMessage;
-	}
+        return chatMessage;
+    }
 
-	private boolean checkRequirements(Chat chat, DummyPlayer dummyPlayer) {
-		return requirementEvaluator.check(chat.getRequirements().get(Participants.RECIPIENT), dummyPlayer);
-	}
+    private boolean checkRequirements(Chat chat, DummyPlayer dummyPlayer) {
+        return requirementEvaluator.check(chat.getRequirements().get(Participants.RECIPIENT), dummyPlayer);
+    }
 
-	private boolean isInSameRealm(DummyPlayer sender, DummyPlayer recipient) {
-		return recipient.getLocation().equals(sender.getLocation());
-	}
+    private boolean isInSameRealm(DummyPlayer sender, DummyPlayer recipient) {
+        if (PlatformType.isProxy())
+            return recipient.getServer().equals(sender.getServer());
 
-	private boolean isWithinRadius(DummyPlayer sender, DummyPlayer recipient, double radius) {
-		return interactor.areWithinRange(sender.getUniqueId(), recipient.getUniqueId(), radius);
-	}
+        return recipient.getLocation().equals(sender.getLocation());
+    }
+
+    private boolean isWithinRadius(DummyPlayer sender, DummyPlayer recipient, double radius) {
+        return interactor.areWithinRange(sender.getUniqueId(), recipient.getUniqueId(), radius);
+    }
 }
