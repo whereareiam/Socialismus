0805a78d7264243494edc2c51e726de46f30e255
whereareiam <mmm.g032@gmail.com>
2025-12-02 21:33:32 +0100

    fix(api): add NoArgsConstructor and AllArgsConstructor to UpdateSource model

--- socialismus-common-api/src/main/java/me/whereareiam/socialismus/model/update/UpdateSource.java
+++ socialismus-common-api/src/main/java/me/whereareiam/socialismus/model/update/UpdateSource.java
@@ -1,11 +1,15 @@
 package me.whereareiam.socialismus.model.update;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import me.whereareiam.socialismus.type.module.ProviderType;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateSource {
 	private ProviderType provider;
 	private String id;
